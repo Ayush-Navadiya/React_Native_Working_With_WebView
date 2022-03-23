63fd2b4462ab36b427c90353a09bc5ea27e4776b
Ayush-Navadiya <ayushnavadiya@gmail.com>
2022-03-23 11:57:37 +0530

    initial Commit

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <WebView
+        source={{
+          uri: 'https://www.buymeacoffee.com/ayushnavadiya'
+        }}
+        style={{ marginTop: 20 }}
+      />
   );
 }
 
